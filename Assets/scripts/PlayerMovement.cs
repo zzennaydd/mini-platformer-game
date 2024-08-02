9717a8ebefe706ce333c3055c4ddda6aa6cb76d6
zzenchery <99723552+zzenchery@users.noreply.github.com>
2024-08-02 18:04:27 +0300

    .

--- Assets/scripts/PlayerMovement.cs
+++ Assets/scripts/PlayerMovement.cs
@@ -5,23 +5,43 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
    private Rigidbody2D rb;
-   
+    private Animator anim;
     // Start is called before the first frame update
    private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     private void Update()
     {
         float dirX = Input.GetAxisRaw("Horizontal");
+       
         rb.velocity = new Vector2(dirX * 7f, rb.velocity.y);
 
         if(Input.GetButtonDown("Jump"))
         {
             rb.velocity = new Vector2(rb.velocity.x, 7f);
         }
+
+        if(dirX > 0f)
+        {
+            anim.SetBool("IsRunning", true);
+        }
+        else if(dirX < 0f)
+        {
+            anim.SetBool("IsRunning", true);
+        }
+        else
+        {
+            anim.SetBool("IsRunning", false);
+        }
+
+       while(rb.velocity.y == 7f)
+        {
+            anim.SetBool("IsJumping", true);
+        }
+
     }
 }
