d2843724a036d4155ec6947c509d2595ea9e8022
zzenchery <99723552+zzenchery@users.noreply.github.com>
2024-08-03 19:07:56 +0300

    .

--- Assets/scripts/PlayerMovement.cs
+++ Assets/scripts/PlayerMovement.cs
@@ -6,9 +6,11 @@ public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
     private SpriteRenderer sprite;
+    private BoxCollider2D coll;
     private Animator anim;
     private float dirX = 0f;
 
+   [SerializeField] private LayerMask jumpableGround;
    [SerializeField] private float moveSpeed = 7f;
    [SerializeField] private float jumpForce = 7f;
     // Start is called before the first frame update
@@ -17,6 +19,7 @@ public class PlayerMovement : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
         sprite = GetComponent<SpriteRenderer>();
+        coll = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
@@ -26,7 +29,7 @@ public class PlayerMovement : MonoBehaviour
        
         rb.velocity = new Vector2(dirX * moveSpeed, rb.velocity.y);
 
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && IsGrounded())
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
         }
@@ -51,6 +54,22 @@ public class PlayerMovement : MonoBehaviour
         {
             anim.SetBool("IsRunning", false);
         }
+
+        if(rb.velocity.y > .1f)
+        {
+            anim.SetBool("IsJumping", true);
+            anim.SetBool("IsRunning", false);
+            
+        }else if (rb.velocity.y < .1f)
         
+        {
+            anim.SetBool("IsJumping", false);
+        }
+        
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .1f,jumpableGround);
     }
 }
