f7e62789ec51009f6a460d8d41813cd0873573a3
zzenchery <zy.aydogan@gmail.com>
2024-11-28 18:59:55 +0300

    .

--- Assets/scripts/StickyPlatform.cs
+++ Assets/scripts/StickyPlatform.cs
@@ -4,15 +4,14 @@ using UnityEngine;
 
 public class StickPlatform : MonoBehaviour
 {
-
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.name == "Player")
+        if (collision.gameObject.name == "Player")
         {
             collision.gameObject.transform.SetParent(transform);
         }
     }
-    private void OnCollisionExit2D(Collision2D collision)
+    private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.gameObject.name == "Player")
         {
