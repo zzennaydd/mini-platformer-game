1c6801715efb5a87ad1df5ba1383e3844d1cac82
zzenchery <zy.aydogan@gmail.com>
2024-12-09 20:32:46 +0300

    .

---
+++ Assets/scripts/Fish.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fish : MonoBehaviour
+{
+    [SerializeField] private GameObject[] waypoints;
+    private int currentWaypointIndex = 0;
+
+    [SerializeField] private float speed = 10f;
+
+    private SpriteRenderer sprite;
+    private void Start()
+    {
+        sprite = gameObject.GetComponent<SpriteRenderer>();
+    }
+    private void Update()
+    {
+        if (Vector2.Distance(waypoints[currentWaypointIndex].transform.position, transform.position) < .1f)
+        {
+            currentWaypointIndex++;
+            if (currentWaypointIndex >= waypoints.Length)
+            {
+                currentWaypointIndex = 0;
+            }
+            UpdateDirection();
+        }
+        transform.position = Vector2.MoveTowards(transform.position, waypoints[currentWaypointIndex].transform.position, Time.deltaTime * speed);
+    }
+    private void UpdateDirection()
+    {
+        if (currentWaypointIndex == 0)
+        {
+            sprite.flipY = true;
+        }
+        else
+            sprite.flipY = false;
+    }
+   
+}
