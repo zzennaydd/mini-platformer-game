3a080813951de0ab8f7d8403055cf2d57330f76e
zzenchery <zy.aydogan@gmail.com>
2025-04-03 19:43:56 +0300

    .

---
+++ Assets/PauseMenu.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] GameObject pauseMenu;
+
+    public void Pause()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0;
+
+    }
+    public void Home()
+    {
+        SceneManager.LoadScene("Start Menu");
+    }
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1;
+    }
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
