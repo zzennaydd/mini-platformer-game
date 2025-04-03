0b23a14bfc500466679cbe6baefb240b9a7af934
zzenchery <zy.aydogan@gmail.com>
2025-04-03 18:40:09 +0300

    .

--- Assets/scripts/Finish.cs
+++ Assets/scripts/Finish.cs
@@ -25,12 +25,22 @@ public class Finish : MonoBehaviour
         Debug.Log("flag is waving.");
 
     }
+    private void UnlockNewLevel()
+    {
+        if (SceneManager.GetActiveScene().buildIndex >= PlayerPrefs.GetInt("ReachedIndex"))
+        {
+            PlayerPrefs.SetInt("ReachedIndex", SceneManager.GetActiveScene().buildIndex + 1);
+            PlayerPrefs.SetInt("UnlockedLevel", PlayerPrefs.GetInt("UnlockedLevel", 1) + 1);
+            PlayerPrefs.Save();
+        }
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
             if (levelFinished)
             {
+                UnlockNewLevel();
                 Invoke("StartNextLevel", 2f);
                 Debug.Log("starting next level now...");
             }
